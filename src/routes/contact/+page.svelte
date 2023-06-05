4aa36fc0f70b58827261c6bd79068c6cc2b1b314
sha5b <ned.tabulov@gmail.com>
2023-06-05 17:16:31 +0200

    Update

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,30 @@
-<div>contact</div>
+<script>
+	export let data;
+	import { getImageURL } from '$lib/utils/getURL';
+</script>
+
+<container>
+	<a href="/"><h1>Kontakt</h1></a>
+</container>
+
+<style>
+	container {
+		gap: 1rem;
+	}
+	img {
+		object-fit: cover;
+		max-height: 500px;
+		width: 100%;
+	}
+	a {
+		text-decoration: none;
+		color: black;
+	}
+	a:hover {
+		color: var(--primary);
+	}
+	h1 {
+		font-size: 5rem;
+		margin-bottom: 0;
+	}
+</style>
